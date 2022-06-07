c2e1cbbbe16ab30eba64fa56ebd5eda9d1cea957
TakashiHamada <hamada@gift10.net>
2022-06-07 12:57:17 +0900

    show the sample image.

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        imageFile: "test.png",
+    },
+    created: function () {
+    },
+    mounted() {
+        document.addEventListener('keydown', this.onKeyDown)
+    },
+    beforeDestroy() {
+        document.removeEventListener('keydown', this.onKeyDown)
+    },
+    methods: {
+    }
+})
+
+// --
+function waitSec(sec) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, sec * 1000);
+    })
+}
