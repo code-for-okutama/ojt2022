32e6028acea2b5701cbc9da9e8b17c863e342f1a
TakashiHamada <hamada@gift10.net>
2022-06-07 15:38:46 +0900

    show choice

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
+var SLIDE_MAX = 1;
+var CHOICE = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+];
+
 var app = new Vue({
     el: '#app',
     data: {
-        imageFile: "test.png",
+        slideIdx: 0,
+        subscript: "init",
+        list: CHOICE,
+        choiceScene: false,
     },
     created: function () {
     },
-    mounted() {
-        document.addEventListener('keydown', this.onKeyDown)
-    },
-    beforeDestroy() {
-        document.removeEventListener('keydown', this.onKeyDown)
-    },
     methods: {
-    }
+        next() {
+            if (this.slideIdx < SLIDE_MAX) {
+                this.slideIdx++;
+            }
+        },
+        get() {
+            return this.list[0];
+        }
+    },
 })
 
 // --
