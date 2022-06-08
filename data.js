561c2f8cf16f9025b21c7efd9547beca940be49a
TakashiHamada <hamada@gift10.net>
2022-06-08 13:49:09 +0900

    フレームを追加（具体的なファイル名はこれから）

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 var SLIDE = [
+    // OP
     {
         id: 0,
         goto: [10],
@@ -17,6 +18,7 @@ var SLIDE = [
         image: "op_island",
         script: ["島が見えてきた"]
     },
+    // 1日目
     {
         id: 100,
         goto: [110],
@@ -53,10 +55,159 @@ var SLIDE = [
         image: "day1_sea",
         script: ["海の中は様々な生物がいたがその他何も見つからなかった"]
     },
+    // 2日目
     {
         id: 200,
-        goto: [0],
+        goto: [210],
         image: "day2_investigation",
         script: ["雨が降っているけど、小説家がいなくなった方へいってみよう"]
+    },
+    {
+        id: 210,
+        goto: [211],
+        image: "day2_`",
+        script: [""]
+    },
+    {
+        id: 211,
+        goto: [220, 230, 240],
+        image: "day2_`",
+        script: [""]
+    },
+    {
+        id: 220,
+        goto: [211],
+        image: "day2",
+        script: [""]
+    },
+    {
+        id: 230,
+        goto: [211],
+        image: "day2",
+        script: [""]
+    },
+    {
+        id: 240,
+        goto: [211],
+        image: "day2",
+        script: [""]
+    },
+    // 3日目
+    {
+        id: 300,
+        goto: [310],
+        image: "",
+        script: [""]
+    },
+    {
+        id: 310,
+        goto: [311],
+        image: "day3_",
+        script: [""]
+    },
+    {
+        id: 311,
+        goto: [320, 330, 340],
+        image: "day3_",
+        script: [""]
+    },
+    {
+        id: 320,
+        goto: [311],
+        image: "day3",
+        script: [""]
+    },
+    {
+        id: 330,
+        goto: [311],
+        image: "day3",
+        script: [""]
+    },
+    {
+        id: 340,
+        goto: [311],
+        image: "day3",
+        script: [""]
+    },
+    // 4日目
+    {
+        id: 400,
+        goto: [410],
+        image: "",
+        script: [""]
+    },
+    {
+        id: 410,
+        goto: [411],
+        image: "day4_",
+        script: [""]
+    },
+    {
+        id: 411,
+        goto: [420, 430, 440],
+        image: "day4_",
+        script: [""]
+    },
+    {
+        id: 420,
+        goto: [411],
+        image: "day4",
+        script: [""]
+    },
+    {
+        id: 430,
+        goto: [411],
+        image: "day4",
+        script: [""]
+    },
+    {
+        id: 440,
+        goto: [411],
+        image: "day3",
+        script: [""]
+    },
+    // 5日目
+    {
+        id: 500,
+        goto: [510],
+        image: "",
+        script: [""]
+    },
+    {
+        id: 510,
+        goto: [511],
+        image: "day5_",
+        script: [""]
+    },
+    {
+        id: 511,
+        goto: [520, 530, 540],
+        image: "day5_",
+        script: [""]
+    },
+    {
+        id: 520,
+        goto: [511],
+        image: "day5",
+        script: [""]
+    },
+    {
+        id: 530,
+        goto: [511],
+        image: "day5",
+        script: [""]
+    },
+    {
+        id: 540,
+        goto: [511],
+        image: "day5",
+        script: [""]
+    },
+    // END
+    {
+        id: 1000,
+        goto: [0],
+        image: "",
+        script: [""]
     },
 ];
